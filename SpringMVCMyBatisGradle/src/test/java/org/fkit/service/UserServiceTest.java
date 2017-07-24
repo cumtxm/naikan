40480f2faa96fb7548da39a3a91c05f3c9e80326
zxm <18035379016@163.com>
2017-07-24 17:05:08 +0800

    test修改6.0

--- SpringMVCMyBatisGradle/src/test/java/org/fkit/service/UserServiceTest.java
+++ SpringMVCMyBatisGradle/src/test/java/org/fkit/service/UserServiceTest.java
@@ -27,18 +27,18 @@ public class UserServiceTest {
 			}
 			
 		}
-		@Test
-		public void testupdatepassword() {
-			User user = UserService.updatepassword
-					("1284517569@163.com","123456");
-			System.out.println("修改成功");
-		}
-		@Test
-		public void testgetuser() {
-			User user=UserService.getuser(2);
-			System.out.println(user.getUsername());
-			System.out.println(user.getemail());
-			System.out.println(user.getPhone());
-		}
+//		@Test
+//		public void testupdatepassword() {
+//			User user = UserService.updatepassword
+//					("1284517569@163.com","123456");
+//			System.out.println("修改成功");
+//		}
+//		@Test
+//		public void testgetuser() {
+//			User user=UserService.getuser(2);
+//			System.out.println(user.getUsername());
+//			System.out.println(user.getemail());
+//			System.out.println(user.getPhone());
+//		}
 	}
 
